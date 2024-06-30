229049e614d4eb3b91ca6daa010d819b1fbfd6e9
Ric3cir121 <ric3cir121@gmail.com>
2024-06-30 16:58:56 +0200

    arras.io is now loaded in arras.io/mod

--- modLoader.js
+++ modLoader.js
@@ -9,8 +9,25 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
-    // Your code here...
+    if(window.location.pathname == "/mod" || window.location.pathname == "/mod/"){
+        window.history.pushState({}, null, "/mod/"+window.location.hash);
+
+        document.head.innerHTML = "<style>body{background-color:#484848;}</style>";
+        document.body.innerHTML = "";
+
+        let main_page = (new DOMParser()).parseFromString(await (await fetch("/")).text(),"text/html");
+
+        // patches to the js have to be applied here
+
+        const main_script = document.createElement("script");
+        main_script.textContent = main_page.body.querySelector("script").textContent;
+
+        // note: there is a script that is supposed to run on the head, but it's not
+        document.head.innerHTML = main_page.head.innerHTML;
+        document.body.innerHTML = "";
+        document.body.appendChild(main_script);
+    }
 })();
