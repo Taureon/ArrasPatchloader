050016781fc060a4c9d941fcaa24896e41b460ac
Ric3cir121 <ric3cir121@gmail.com>
2024-07-04 12:38:18 +0200

    split patchloader.js in .user.js and .meta.js

---
+++ patchloader.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Arras.io Patchloader
+// @namespace    arras.io.patchloader
+// @version      2024-07-04
+// @description  An userscript that loads script patches for Arras.io
+// @author       https://github.com/Taureon, https://github.com/Ric3cir121
+// @match        *://arras.io/mod*
+// @icon         https://arras.io/favicon/base.svg
+// @grant        none
+// @downloadURL https://raw.githubusercontent.com/Taureon/ArrasPatchloader/main/patchloader.user.js
+// @updateURL https://raw.githubusercontent.com/Taureon/ArrasPatchloader/main/patchloader.meta.js
+// ==/UserScript==
